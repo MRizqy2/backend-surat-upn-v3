18eb9458c2d6d0213141ba153bed5d6a31d2fb5e
rizal15D <muhammadrizal15122001@gmail.com>
2024-01-15 13:41:24 +0700

    update periode status

--- src/Http/controllers/periode_controller/periode_controller.js
+++ src/Http/controllers/periode_controller/periode_controller.js
@@ -9,6 +9,6 @@ const app = express.Router();
 app.get("/", getPeriode);
 app.post("/", postPeriode);
 app.put("/tahun", putPeriodeTahun);
-app.put("/status", putPeriodeTahun);
+app.put("/status", putPeriodeStatus);
 
 module.exports = app;
