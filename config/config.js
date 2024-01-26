44dbfc42a7b6c1fb12cb9b38f8cf1dba508f9278
rizal15D <muhammadrizal15122001@gmail.com>
2024-01-26 10:09:29 +0700

    merge ocr

--- config/config.js
+++ config/config.js
@@ -5,7 +5,6 @@ let stringUser = String(process.env.POSTGRES_USER);
 
 let database;
 
-<<<<<<< HEAD
 if (process.env.POSTGRES_URL) {
   database = {
     url: `postgres://${stringUser}:${stringPassword}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}`,
@@ -24,41 +23,12 @@ if (process.env.POSTGRES_URL) {
   };
 } else {
   database = {
-=======
-module.exports = {
-  development: {
-    // url: `postgres://${stringUser}:${stringPassword}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}`,
-    // host: process.env.POSTGRES_HOST,
-    // username: stringUser,
-    // password: stringPassword,
-    // database: process.env.POSTGRES_DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     secret_key: process.env.SECRET_KEY,
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    // },
-  },
-  test: {
-    // url: `postgres://${stringUser}:${stringPassword}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}`,
-    // host: process.env.POSTGRES_HOST,
-    // username: stringUser,
-    // password: stringPassword,
-    // database: process.env.POSTGRES_DATABASE,
->>>>>>> ocr
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    secret_key: process.env.SECRET_KEY,
-<<<<<<< HEAD
   };
 }
 
@@ -68,32 +38,4 @@ module.exports = {
   test: database,
 
   production: database,
-=======
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    // },
-  },
-  production: {
-    // url: `postgres://${stringUser}:${stringPassword}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}`,
-    // host: process.env.POSTGRES_HOST,
-    // username: stringUser,
-    // password: stringPassword,
-    // database: process.env.POSTGRES_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    secret_key: process.env.SECRET_KEY,
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    // },
-  },
->>>>>>> ocr
 };
