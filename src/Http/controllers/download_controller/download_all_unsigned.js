1d7f23b88826f8e8d00169371ba27a882d76dd88
Rizqy2 <machinegt22@gmail.com>
2024-07-16 18:11:32 +0700

    update download zip

--- src/Http/controllers/download_controller/download_all_unsigned.js
+++ src/Http/controllers/download_controller/download_all_unsigned.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { DAFTAR_SURAT, USERS, JABATAN, STATUS } = require("../../../models");
+const {
+  DAFTAR_SURAT,
+  USERS,
+  JABATAN,
+  STATUS,
+  AKSES_SURAT,
+} = require("../../../models");
 const fs = require("fs");
 const { Op } = require("sequelize");
 const path = require("path");
@@ -14,9 +20,13 @@ const downloadAllUnsigned = async (req, res) => {
     const user_jabatan = await JABATAN.findOne({
       where: { id: user.jabatan_id },
     });
+    console.log("dada",user_jabatan.name);
 
     const daftarSurat = await DAFTAR_SURAT.findAll({
-      where: { "$status.status$": { [Op.like]: `%${user_jabatan.name}%` } },
+      where: {
+        "$status.status$": { [Op.like]: `%Di Daftar Tunggu ${user_jabatan.name}%` },
+        visible: true,
+      },
       include: [
         {
           model: STATUS,
@@ -24,8 +34,9 @@ const downloadAllUnsigned = async (req, res) => {
         },
       ],
     });
-
+    console.log("dadaaaaaas",user_jabatan.name);
     const alamats = daftarSurat.map((surat) => surat.path);
+    console.log("asdwdad", alamats);
 
     const formattedPathsPromises = alamats.map(async (file_path, index) => {
       file_path = decodeURIComponent(file_path);
@@ -60,6 +71,7 @@ const downloadAllUnsigned = async (req, res) => {
     });
 
     const formattedPaths = await Promise.all(formattedPathsPromises);
+    console.log("dwadwadwad", formattedPaths);
 
     res.header("Content-Type", "application/zip");
     res.zip({
