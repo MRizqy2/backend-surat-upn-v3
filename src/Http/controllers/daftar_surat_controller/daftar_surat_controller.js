576d31f0dce917fb63417e31e77fde7e7acc2d16
rizal15D <muhammadrizal15122001@gmail.com>
2024-07-21 13:51:50 +0700

    visible false surat

--- src/Http/controllers/daftar_surat_controller/daftar_surat_controller.js
+++ src/Http/controllers/daftar_surat_controller/daftar_surat_controller.js
@@ -4,11 +4,13 @@ const { deleteSurat } = require("./delete_daftar_surat");
 const { getDaftarSurat } = require("./get_daftar_surat");
 
 const express = require("express");
+const { visibleFalseSurat } = require("./visible_false_surat");
 const app = express.Router();
 
 app.get("/", getDaftarSurat);
 app.use("/multer", multerController);
 app.use("/multer", perbaikanController);
 app.delete("/delete", deleteSurat);
+app.delete("/hide", visibleFalseSurat);
 
 module.exports = app;
